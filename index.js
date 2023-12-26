9e731269b26c2c28b095818a6bc49027898b839b
purvik6062 <purvikpanchal6336@gmail.com>
2023-12-26 13:09:58 +0530

    updated bot with database

--- index.js
+++ index.js
@@ -19,6 +19,39 @@ const { Routes } = require('discord-api-types/v9');
 const passport = require("passport");
 const DiscordStrategy = require("passport-discord").Strategy;
 
+
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('discordBotData.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, async (err) => {
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Database opened');
+        await initialize();
+    }
+});
+
+
+async function createTables() {
+    return new Promise((resolve, reject) => {
+        db.run(`
+CREATE TABLE IF NOT EXISTS userData (
+  member_user_id TEXT,
+  inviter_user_id TEXT,
+  member_joinedTimestamp TEXT
+)
+`, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log('userData table created');
+                resolve();
+            }
+
+        })
+    });
+}
+
 client.once('ready', () => {
     console.log('Bot is ready!');
     // checkInvitesForAllMembers();
@@ -61,7 +94,7 @@ client.on('interactionCreate', async (interaction) => {
 
     if (commandName === 'count') {
         console.log("interaction.channelId", interaction.channelId);
-        console.log("process.env.INVITE_AMB_ROLE_ID", process.env.INVITE_AMB_CHANNEL_ID);
+        console.log("process.env.INVITE_AMB_CHANNEL_ID", process.env.INVITE_AMB_CHANNEL_ID);
 
         if (interaction.channelId === process.env.INVITE_AMB_CHANNEL_ID) {
             await getInviteCount(interaction);
@@ -88,6 +121,52 @@ client.on('interactionCreate', async (interaction) => {
     }
 });
 
+async function getUniqueInvitedMembers(userId) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT COUNT(DISTINCT member_user_id) AS uniqueInviteCount, GROUP_CONCAT(DISTINCT member_user_id) AS uniqueMemberIds FROM userData WHERE inviter_user_id = ?`;
+
+        db.get(query, [userId], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                const result = {
+                    uniqueInviteCount: row ? row.uniqueInviteCount : 0,
+                    uniqueMemberIds: row ? (row.uniqueMemberIds || '').split(',') : [],
+                };
+                resolve(result);
+            }
+        });
+    });
+}
+
+async function checkRegularMembers(uniqueMemberIds) {
+    const results = [];
+
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+
+    if (guild) {
+        await guild.members.fetch();
+
+        for (const memberId of uniqueMemberIds) {
+            const member = guild.members.cache.get(memberId);
+
+            if (member) {
+                results.push({
+                    memberId,
+                    isRegular: true,
+                });
+            } else {
+                results.push({
+                    memberId,
+                    isRegular: false,
+                });
+            }
+        }
+    }
+
+    return results;
+}
+
 async function getInviteCount(interaction) {
     const member = interaction.guild.members.cache.get(interaction.user.id);
     const invites = await interaction.guild.invites.fetch();
@@ -100,7 +179,17 @@ async function getInviteCount(interaction) {
         }
     });
 
-    interaction.reply(`You have a total of ${totalInviteCount} invite(s).`);
+    const userId = interaction.user.id;
+
+    const { uniqueInviteCount, uniqueMemberIds } = await getUniqueInvitedMembers(userId);
+
+    console.log(uniqueMemberIds)
+
+    const result = await checkRegularMembers(uniqueMemberIds);
+    console.log(result)
+
+    interaction.reply(`You have invited ${uniqueInviteCount} member(s).`);
+    // interaction.reply(`You have a total of ${totalInviteCount} invite(s).`);
 }
 
 async function assignRoleIfEnoughInvites(interaction) {
@@ -110,12 +199,16 @@ async function assignRoleIfEnoughInvites(interaction) {
     let totalInviteCount = 0;
 
     invites.forEach((invite) => {
-        if (invite.inviter && invite.inviterID === interaction.user.id) {
+        if (invite.inviter && invite.inviter.id === interaction.user.id) {
             totalInviteCount += invite.uses;
         }
     });
 
-    if (totalInviteCount >= 5) {
+    const userId = interaction.user.id;
+
+    const { uniqueInviteCount, uniqueMemberIds } = await getUniqueInvitedMembers(userId);
+
+    if (uniqueInviteCount >= 5) {
         // Replace 'ROLE_ID_HERE' with the actual ID of the role you want to assign
         const roleId = process.env.INVITE_AMB_ROLE_ID;
         const role = interaction.guild.roles.cache.get(roleId);
@@ -128,7 +221,7 @@ async function assignRoleIfEnoughInvites(interaction) {
             interaction.reply('Error: Role not found.');
         }
     } else {
-        interaction.reply(`You need at least 5 invites (total: ${totalInviteCount}) to get the role.`);
+        interaction.reply(`You need at least 5 invites to get the role.`);
     }
 }
 
@@ -149,10 +242,15 @@ async function checkInvitesForAllMembers() {
 async function assignRoleIfEnoughInvitesOnJoin(member) {
     const invites = await member.guild.invites.fetch();
 
-    // Assuming you have only one invite link for simplicity
-    const userInvites = invites.find((invite) => invite.inviter && invite.inviter.id === member.user.id);
+    let totalInviteCount = 0;
 
-    if (userInvites && userInvites.uses >= 5) {
+    invites.forEach((invite) => {
+        if (invite.inviter && invite.inviter.id === interaction.user.id) {
+            totalInviteCount += invite.uses;
+        }
+    });
+
+    if (totalInviteCount >= 5) {
         // Replace 'ROLE_ID_HERE' with the actual ID of the role you want to assign
         const roleId = process.env.INVITE_AMB_ROLE_ID;
         const role = member.guild.roles.cache.get(roleId);
@@ -163,6 +261,48 @@ async function assignRoleIfEnoughInvitesOnJoin(member) {
         } else {
             console.error('Error: Role not found.');
         }
+    }
+}
+
+
+client.on('guildMemberAdd', async (member) => {
+
+    const guild = member.guild;
+    const auditLogs = await guild.fetchAuditLogs({ type: 28 }); // Use the integer value for GUILD_MEMBER_ADD
+
+    const memberAddEntry = auditLogs.entries.first();
+
+    if (memberAddEntry) {
+        const { executor, target } = memberAddEntry;
+
+        const logChannel = guild.channels.cache.get(process.env.INVITE_AMB_CHANNEL_ID);
+
+        if (logChannel) {
+
+            const dataInsertSql = `INSERT INTO userData VALUES (?, ?, ?)`;
+
+            db.run(dataInsertSql, [
+                member.user.id,
+                executor.id,
+                member.joinedAt,
+            ]);
+
+            const joinedAt = member.joinedAt;
+            const localTime = joinedAt.toLocaleString();
+            logChannel.send(`Member: ${member.user.tag} has joined. Invited by: ${executor.tag}.`);
+            // logChannel.send(`Member: ${member.user.tag} (${member.displayName}) (${member.user.id}) has joined. Invited by: ${executor.tag} (${executor.id}). Joined at: ${localTime}`);
+        } else {
+            console.error('Error: Log channel not found.');
+        }
+    }
+});
+
+
+async function initialize() {
+    try {
+        await createTables();
+    } catch (error) {
+        console.error('Initialization error:', error);
     }
 }
 
